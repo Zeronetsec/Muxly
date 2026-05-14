8f87b270de5bfd1ffdd23e386d1652fd863e1fc6
Zeronetsec <senxhunter996@gmail.com>
2026-05-14 12:18:30 +0700

    add: metadata/cursor/chcursor.json
    add: metadata/cursor/chcursor_blink_rate.json
    
    add: metadata/cursor/reset_cursor.json
    
    add: metadata/cursor/reset_cursor_blink_rate.json
    
    add: metadata/font/chfont.json
    
    add: metadata/font/list_fonts.json
    
    add: metadata/font/reset_fonts.json
    
    add: metadata/general/help.json
    
    add: metadata/general/info.json
    
    add: metadata/general/version.json
    
    add: metadata/input/volume_keys.json
    
    add: metadata/rootfs/install_rootfs.json
    
    add: metadata/rootfs/list_installed_rootfs.json
    
    add: metadata/rootfs/list_rootfs.json
    
    add: metadata/rootfs/remove_rootfs.json
    
    add: metadata/rootfs/reset_rootfs.json
    
    add: metadata/rootfs/rootfs.json
    
    add: metadata/rootfs/rootfs_command.json
    
    add: metadata/rootfs/symlink.json
    
    add: metadata/screen/disable_session_toast.json
    
    add: metadata/screen/fullscreen.json
    
    add: metadata/screen/fullscreen_workaround.json
    
    add: metadata/screen/reset_transcript_rows.json
    
    add: metadata/screen/transcript_rows.json
    
    add: metadata/shortcut/reset_shortcut.json
    
    add: metadata/shortcut/shortcut_create_session.json
    
    add: metadata/shortcut/shortcut_next_session.json
    
    add: metadata/shortcut/shortcut_previous_session.json
    
    add: metadata/shortcut/shortcut_rename_session.json
    
    add: metadata/theme/chtheme.json
    
    add: metadata/theme/force_black_ui.json
    
    add: metadata/theme/list_themes.json
    
    add: metadata/theme/reset_theme.json
    
    modified: utils/general/help.sh

--- utils/general/help.sh
+++ utils/general/help.sh
@@ -1,68 +1,37 @@
 # https://github.com/Zeronetsec/Muxly
 
 function __help__() {
-    __birthday__
+    local metadata="${muxlyroot}/metadata"
 
     echo -e "${N}Usage: ${GG}muxly ${CC}<command> [<args>]${N}"
-
     printf '\n'
-    echo -e "${N}Font commands:"
-    echo -e "    ${R}› ${GG}--chfont ${DG}<${CC}font${DG}> - ${YY}change fonts${N}"
-    echo -e "    ${R}› ${GG}--list-fonts ${DG}- ${YY}show available fonts${N}"
-    echo -e "    ${R}› ${GG}--reset-font ${DG}- ${YY}reset font to default${N}"
 
-    printf '\n'
-    echo -e "${N}Theme commands:"
-    echo -e "    ${R}› ${GG}--chtheme ${DG}<${CC}theme${DG}> - ${YY}change theme${N}"
-    echo -e "    ${R}› ${GG}--list-themes ${DG}- ${YY}show available themes${N}"
-    echo -e "    ${R}› ${GG}--reset-theme ${DG}- ${YY}reset theme to default${N}"
-    echo -e "    ${R}› ${GG}--force-black-ui ${DG}<${CC}true${DG}|${CC}false${DG}> - ${YY}force black ui mode${N}"
+    idx=0
+    shopt -s nullglob
 
-    printf '\n'
-    echo -e "${N}Cursor commands:"
-    echo -e "    ${R}› ${GG}--chcursor ${DG}<${CC}block${DG}|${CC}underline${DG}|${CC}bar${DG}> - ${YY}change cursor style${N}"
-    echo -e "    ${R}› ${GG}--chcursor-blink-rate ${DG}<${CC}0${DG}-${CC}2000${DG}> - ${YY}set cursor blink rate${N}"
-    echo -e "    ${R}› ${GG}--reset-cursor ${DG}- ${YY}reset cursor style${N}"
-    echo -e "    ${R}› ${GG}--reset-cursor-blink-rate ${DG}- ${YY}reset blink rate${N}"
+    for dir in "${metadata}/"*/; do
+        [[ "${idx}" -ne 0 ]] && printf '\n'
 
-    printf '\n'
-    echo -e "${N}Screen commands:"
-    echo -e "    ${R}› ${GG}--fullscreen ${DG}<${CC}true${DG}|${CC}false${DG}> - ${YY}toggle fullscreen${N}"
-    echo -e "    ${R}› ${GG}--fullscreen-workaround ${DG}<${CC}true${DG}|${CC}false${DG}> - ${YY}enable fullscreen fix${N}"
-    echo -e "    ${R}› ${GG}--transcript-rows ${DG}<${CC}1${DG}-${CC}50000${DG}> - ${YY}set scrollback lines${N}"
-    echo -e "    ${R}› ${GG}--disable-session-toast ${DG}<${CC}true${DG}|${CC}false${DG}> - ${YY}disable session notifications${N}"
-    echo -e "    ${R}› ${GG}--reset-transcript-rows ${DG}- ${YY}reset scrollback lines to default${N}"
+        section="$(command basename "${dir}")"
+        echo -e "${N}${section^} commands:"
 
-    printf '\n'
-    echo -e "${N}Shortcut commands:"
-    echo -e "    ${R}› ${GG}--shortcut-create-session ${DG}<${CC}value${DG}> - ${YY}set create session shortcut${N}"
-    echo -e "    ${R}› ${GG}--shortcut-next-session ${DG}<${CC}value${DG}> - ${YY}set next session shortcut${N}"
-    echo -e "    ${R}› ${GG}--shortcut-previous-session ${DG}<${CC}value${DG}> - ${YY}set previous session shortcut${N}"
-    echo -e "    ${R}› ${GG}--shortcut-rename-session ${DG}<${CC}value${DG}> - ${YY}set rename session shortcut${N}"
-    echo -e "    ${R}› ${GG}--reset-shortcut ${DG}- ${YY}reset all shortcuts${N}"
+        for file in "${dir}"/*.json; do
+            command="$(command jq -r '.Command' "${file}")"
+            args="$(command jq -r '.Args' "${file}")"
+            desc="$(command jq -r '.Description' "${file}")"
 
-    printf '\n'
-    echo -e "${N}Rootfs commands:"
-    echo -e "    ${R}› ${GG}--rootfs ${DG}<${CC}distro${DG}> - ${YY}switch root filesystem${N}"
-    echo -e "    ${R}› ${GG}--install-rootfs ${DG}<${CC}distro${DG}> - ${YY}install a root filesystem${N}"
-    echo -e "    ${R}› ${GG}--list-rootfs ${DG}- ${YY}show available rootfs options${N}"
-    echo -e "    ${R}› ${GG}--list-installed-rootfs ${DG}- ${YY}show installed rootfs${N}"
-    echo -e "    ${R}› ${GG}--remove-rootfs ${DG}<${CC}distro${DG}> - ${YY}remove an installed root filesystem${N}"
-    echo -e "    ${R}› ${GG}--reset-rootfs ${DG}<${CC}distro${DG}> - ${YY}reset a root filesystem${N}"
-    echo -e "    ${R}› ${GG}--rootfs-command ${DG}<${CC}distro${DG}> <${CC}command${DG}> - ${YY}execute a command inside a specific rootfs${N}"
-    echo -e "    ${R}› ${GG}--symlink ${DG}<${CC}distro${DG}> <${CC}command${DG}> - ${YY}create a host symlink for a rootfs command${N}"
+            if [[ -n "${args}" ]]; then
+                fullcmd="${GG}${command} ${CC}${args}${N}"
+            else
+                fullcmd="${GG}${command}${N}"
+            fi
 
-    printf '\n'
-    echo -e "${N}Input commands:"
-    echo -e "    ${R}› ${GG}--volume-keys ${DG}<${CC}value${DG}> - ${YY}configure volume key behavior${N}"
+            echo -e "    ${R}› ${fullcmd} ${DG}- ${WW}${desc}${N}"
+        done
+        ((idx++))
+    done
 
-    printf '\n'
-    echo -e "${N}General commands:"
-    echo -e "    ${R}› ${GG}--info ${DG}- ${YY}show muxly configurations${N}"
-    echo -e "    ${R}› ${GG}--help ${DG}- ${YY}show help${N}"
-    echo -e "    ${R}› ${GG}--version ${DG}- ${YY}show version${N}"
-
-    return 0
+    shopt -u nullglob
 }
 
 # Copyright (c) 2026 Zeronetsec
