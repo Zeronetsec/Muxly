a4d8cf0d47b459956019e8feeaf29b9ed501683b
Zeronetsec <senxhunter996@gmail.com>
2026-05-21 05:13:45 +0700

    modified: utils/general/version.sh

--- utils/general/version.sh
+++ utils/general/version.sh
@@ -1,12 +1,12 @@
 # https://github.com/Zeronetsec/Muxly
 
 function __version__() {
-    local tools="Muxly"
+    local name="Muxly"
     local version="v0.1"
     local creator="Zeronetsec"
     local homepage="https://github.com/Zeronetsec/Muxly"
 
-    echo -e "${N}Tools: ${GG}${tools}${N}"
+    echo -e "${N}Name: ${GG}${name}${N}"
     echo -e "${N}Version: ${GG}${version}${N}"
     echo -e "${N}Creator: ${GG}${creator}${N}"
     echo -e "${N}Homepage: ${GG}${homepage}${N}"
