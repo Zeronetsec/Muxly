4e51bf2395c676e4a078c49adf0a2c48d043c6a8
Zeronetsec <senxhunter996@gmail.com>
2026-07-28 01:26:06 +0700

    modified: .gitaction/reqgen.py
    modified: .github/workflows/reqgen.yaml

--- .gitaction/reqgen.py
+++ .gitaction/reqgen.py
@@ -7,17 +7,25 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 input_file = ROOT_DIR / ".install" / "extern" / "python_packages.txt"
 output_file = ROOT_DIR / "requirements.txt"
+outvendor_file = ROOT_DIR / "vendor" / "pd530" / "requirements.txt"
 
 try:
     content = input_file.read_text(encoding="utf-8")
     output_file.write_text(content, encoding="utf-8")
     print(f"[+] Generated: {output_file.name}")
+
+    outvendor_file.parent.mkdir(
+        parents=True, exist_ok=True,
+    )
+
+    outvendor_file.write_text(content, encoding="utf-8")
+    print(f"[+] Generated: {outvendor_file.name}")
     sys.exit(0)
 
 except FileNotFoundError:
-    print(f"[!] File: {input_file} not found!")
+    print(f"[!] File not found: {input_file}")
     sys.exit(1)
 
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"[!] Error: {e}")
     sys.exit(1)
